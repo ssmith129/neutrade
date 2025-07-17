6726f7809dc46db752d8ced744ef51d3948c1363
ssmith129 <111539897+ssmith129@users.noreply.github.com>
2025-07-17 15:02:09 -0700

    Disable SWC and use Babel compilation

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: false
+  swcMinify: false,
+  experimental: {
+    forceSwcTransforms: false,
+  },
+  compiler: {
+    // Disable SWC compiler completely
+    removeConsole: false,
+  },
+  // Force Babel usage
+  webpack: (config, { dev, isServer }) => {
+    // Disable SWC loader
+    config.module.rules.forEach((rule) => {
+      if (rule.use && rule.use.loader === 'next-swc-loader') {
+        rule.use.loader = 'babel-loader';
+      }
+    });
+    return config;
+  }
 };
 
 export default nextConfig;
