43723a62062a2be63cb7398ec35fa5ea7ace5f65
ssmith129 <111539897+ssmith129@users.noreply.github.com>
2025-07-17 15:02:57 -0700

    Simplify Next.js configuration to avoid SWC

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  swcMinify: false,
-  experimental: {
-    forceSwcTransforms: false,
-  },
-  compiler: {
-    // Disable SWC compiler completely
-    removeConsole: false,
-  },
-  // Force Babel usage
-  webpack: (config, { dev, isServer }) => {
-    // Disable SWC loader
-    config.module.rules.forEach((rule) => {
-      if (rule.use && rule.use.loader === 'next-swc-loader') {
-        rule.use.loader = 'babel-loader';
-      }
-    });
-    return config;
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
